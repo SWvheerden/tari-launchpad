a4444cf3d340bcb5d461b786c2fb2b3e9a719322
Denis Kolodin <deniskolodin@gmail.com>
2023-07-21 22:49:10 +0200

    feat: add improved cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,26 +1,11 @@
-mod app;
-mod dashboard;
-mod scenes;
-
-use std::env;
-
-use anyhow::{Context, Error};
-use app::App;
-use dashboard::Dashboard;
-use tari_sdm_assets::configurator::Configurator;
+use anyhow::Error;
+use tact::Actor;
+use tari_lp_cli::supervisor::Supervisor;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let mut configurator = Configurator::init()?;
-    configurator.clean_configuration().await?;
-    configurator.init_configuration().await?;
-
-    let workdir = configurator.base_path();
-    env::set_current_dir(workdir)?;
-
-    log4rs::init_file("config/log4rs-cli.yml", Default::default()).context("Can't read a logs configuration file")?;
-
-    let mut app = App::init()?;
-    app.routine().await?;
+    let supervisor = Supervisor::new();
+    let mut addr = supervisor.start();
+    addr.join().await?;
     Ok(())
 }
